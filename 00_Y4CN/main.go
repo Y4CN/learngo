6ad89aa7180d6e37d23a348010ab9c1d8a70e204
Yasin <vyasin96@gmail.com>
2023-05-13 11:31:59 +0330

    add StringConv in Y4CN Folder

--- 00_Y4CN/main.go
+++ 00_Y4CN/main.go
@@ -8,4 +8,11 @@ import (
 func main() {
 	//get CPU CORE number
 	fmt.Println(runtime.NumCPU())
+
+	//StringConvTest
+	// thisIsBool := "gg"
+	// reallBool, e := strconv.ParseBool(thisIsBool)
+	// fmt.Println(e)
+	// fmt.Printf("this should return Boolian Type => %T , value is %t \n", reallBool, reallBool)
+
 }
