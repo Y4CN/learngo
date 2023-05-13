8f025e5a62e4e5830ba8114b683f76fb9cb79350
Yasin <vyasin96@gmail.com>
2023-05-13 12:04:20 +0330

    add new lines in Y4CN

--- 00_Y4CN/main.go
+++ 00_Y4CN/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"runtime"
+	"strconv"
 )
 
 func main() {
@@ -15,4 +17,16 @@ func main() {
 	// fmt.Println(e)
 	// fmt.Printf("this should return Boolian Type => %T , value is %t \n", reallBool, reallBool)
 
+	//I love Practice
+	// fArg := os.Args[1]
+	// lArg := os.Args[2]
+	// fFloatArg, _ := strconv.ParseFloat(fArg, 64)
+	// lFloatArg, _ := strconv.ParseFloat(lArg, 64)
+	// fmt.Println(fFloatArg + lFloatArg)
+
+	//change C to F
+	cel := os.Args[1]
+	fCel, _ := strconv.ParseFloat(cel, 64)
+	far := fCel*1.8 + 32
+	fmt.Printf("%gF\n", far)
 }
