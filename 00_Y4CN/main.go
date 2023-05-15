12f46793a9f9f2afb0abf7a6d06027a9b0ff86e6
Yasin <vyasin96@gmail.com>
2023-05-15 18:08:00 +0330

    if & switch & for is Finish

--- 00_Y4CN/main.go
+++ 00_Y4CN/main.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
 	//get CPU CORE number
 	// fmt.Println(runtime.NumCPU())
@@ -42,4 +46,74 @@ func main() {
 	//Create type
 	// type Duration int64
 	// var d Duration
+
+	//if StateMent Chalenge
+	// const userName1 = "Y4CN"
+	// const userName2 = "Y4CN2"
+	// const pass1 = "12345678"
+	// const pass2 = "123456789"
+	// fmt.Println(len(os.Args))
+	// if len(os.Args) <= 2 {
+	// 	fmt.Println("Plz Enter Your Username & Password")
+	// 	return
+	// }
+	// if (os.Args[1] == userName1 && os.Args[2] == pass1) || (os.Args[1] == userName2 && os.Args[2] == pass2) {
+	// 	if os.Args[1] == userName1 {
+	// 		if os.Args[2] != pass1 {
+	// 			fmt.Printf("Pass is invalid %s\n", userName1)
+	// 			return
+	// 		}
+	// 		fmt.Printf("Welcome %s with a %s password\n", userName1, pass1)
+	// 		return
+	// 	}
+	// 	if os.Args[1] == userName2 {
+	// 		if os.Args[2] != pass2 {
+	// 			fmt.Printf("Pass is invalid %s\n", userName2)
+	// 			return
+	// 		}
+	// 		fmt.Printf("Welcome %s with a %s password\n", userName2, pass2)
+	// 		return
+	// 	}
+	// } else {
+	// 	fmt.Println("You Pass OR User is Invalid")
+	// }
+	// n, err := strconv.Atoi("22")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println(n)
+	// fmt.Printf("Type n is : %T\n", n)
+
+	//chalenge Switch with time package
+	// myTime := time.Now().Hour()
+	// fmt.Println(myTime)
+	// switch {
+	// case myTime >= 0 && myTime < 10:
+	// 	fmt.Println("Good Morning")
+	// case myTime >= 10 && myTime < 15:
+	// 	fmt.Println("Good Noon")
+	// case myTime >= 15 && myTime < 21:
+	// 	fmt.Println("Good Afternoon")
+	// default:
+	// 	fmt.Println("Good Night")
+	// }
+
+	//for Loop
+	const max = 5
+	fmt.Printf("%5s", "X")
+	for i := 0; i <= max; i++ {
+		fmt.Printf("%5d", i)
+	}
+
+	for i := 0; i <= max; i++ {
+		if i == 0 {
+			fmt.Println()
+		}
+		fmt.Printf("%5d", i)
+		for j := 0; j <= max; j++ {
+			fmt.Printf("%5d", i*j)
+		}
+		fmt.Println()
+	}
+
 }
